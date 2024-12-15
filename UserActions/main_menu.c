9580dadd90899445c40ec08bf548c7d770eefe63
ticinovicsara <ticinovicsara0@gmail.com>
2024-12-15 15:01:30 +0100

    added main menu

---
+++ UserActions/main_menu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printMainMenu() {
+    printf("\n===== Hospital Management System =====\n");
+    printf("1 - Dodaj pacijenta\n");
+    printf("2 - Obriši pacijenta\n");
+    printf("3 - Pretraži pacijente\n");
+    printf("4 - Rezerviraj termin kod doktora\n");
+    printf("5 - Upravljanje hitnim slučajevima\n");
+    printf("6 - Izlaz\n");
+}
